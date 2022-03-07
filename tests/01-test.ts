49a09817c12388130749bbe2ecfc1a49f1d6ed03
Benoit Rognier <benoit.rognier@edukera.com>
2022-03-07 19:53:40 +0100

    fix unti test

--- tests/01-test.ts
+++ tests/01-test.ts
@@ -1,31 +1,49 @@
 import { runCrank } from '../src';
 import { register_TestEvent, TestEvent } from './test_bindings_gen';
-
-import { importKey } from '@taquito/signer';
+import { InMemorySigner, importKey } from '@taquito/signer';
 import { TezosToolkit } from '@taquito/taquito';
+import { BlockResponse, RpcClient } from '@taquito/rpc';
+import fs from 'fs';
 
 const Tezos = new TezosToolkit('https://hangzhounet.api.tez.ie');
-importKey(Tezos, "p2sk2obfVMEuPUnadAConLWk7Tf4Dt3n4svSgJwrgpamRqJXvaYcg1")
 
-const bulb = "KT19EAMugKU416cbA9jL1XcukWArfpv4dLYu"
+Tezos.setProvider({
+  signer: new InMemorySigner('YOUR_PRIVATE_KEY'),
+});
+
+let client  = new RpcClient('https://hangzhounet.api.tez.ie');
+
+const event_test_michelson = fs.readFileSync('./contracts/testevent.tz').toString();
+
+const anint = 12345
+const astring = 'This is a string test'
 
 function handleTestEvent(e : TestEvent) {
-  console.log("Test Event detected: " + e.ival + " on " + e.sval);
+  console.log(`Test Event detected with values ${e.ival} and ${e.sval}!`);
+  if (e.ival.toNumber() !== anint || e.sval !== astring) {
+    process.exit(-1)
+  }
+  process.exit()
 }
 
-//registerMyEvent("KT1PPV3GpPU4ofRkSnzC7tzazCvjy3NhigA7",  handle)
-register_TestEvent(bulb, handleTestEvent)
-
-async function testRun () {
-  await runCrank();
+const runTest = async () => {
+  const originationOp = await Tezos.contract.originate({
+    code: event_test_michelson,
+    storage: {}
+  });
+  console.log(`Waiting for confirmation of origination for ${originationOp.contractAddress}...`);
+  const contract = await originationOp.contract();
+  console.log(`Origination completed.`);
+  console.log(`Contract originated at ${contract.address}.`);
+  register_TestEvent(contract.address, handleTestEvent);
+  let bottomBlock : BlockResponse = await client.getBlock({ block: "head" });
+  let bottom = bottomBlock.hash;
+  console.log(`Bottom block hash is ${bottom}`);
+  const op = await contract.methods.default(anint, astring).send();
+  console.log("Calling contract ...");
+  await op.confirmation();
+  console.log("Running Crank ...");
+  runCrank({ bottom : bottom });
 }
 
-
-
-testRun();
-
-
-
-
-
-
+runTest()
